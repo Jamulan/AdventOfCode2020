9e0d72a5e184699d12b21908d8f37b212f7caa00
jamulan <jamulan@ijuggle5.com>
2020-12-07 02:58:49 -0600

    Day 07 part 1 complete

--- 07/main.cpp
+++ 07/main.cpp
@@ -4,16 +4,80 @@
 #include "main.h"
 
 int main() {
-    vector<int> input = {};
+    rules = {};
+    vector<string> keys = {};
     fstream inputFile;
     inputFile.open("input", ios::in);
 
     while (!inputFile.eof()) {
-        int n;
+        Rule rule;
+        string n, key;
+        inputFile >> key;
+        if(key == "") {
+            break;
+        }
         inputFile >> n;
-        input.push_back(n);
+        key.append(' ' + n);
+        inputFile >> n; // "bags"
+        inputFile >> n; // "contain"
+        bool go = true;
+        while (go) {
+            inputFile >> n;
+            if (n == "no") {
+                rule.empty = true;
+                go = false;
+                char test = ' ';
+                do {
+                    test = inputFile.get();
+                } while(test != '.');
+            } else {
+                rule.empty = false;
+                rule.innerBagNum.push_back(stoi(n));
+                string value;
+                inputFile >> value;
+                inputFile >> n;
+                value.append(' ' + n);
+                rule.innerBagCol.push_back(value);
+                inputFile >> n;
+                go = n[n.size() - 1] == ','; // bag/bags ,/.
+            }
+        }
+        if(key != "shiny gold") {
+            rules[key] = rule;
+            keys.push_back(key);
+        }
     }
+    int numBags = 0;
+    for(string key : keys) {
+        if(rules[key].empty) continue;
+        checkedBags = {};
+        vector<string> bags = findInnerBags(key);
+        for(string s : bags) {
+            if(s == "shiny gold") {
+                numBags++;
+                break;
+            }
+        }
+    }
+    cout << numBags << '\n';
     printf("hello world");
 
     return 0;
+}
+
+vector<string> findInnerBags(string bag) {
+    if(rules[bag].empty) return {};
+    for(string s : checkedBags) {
+        if(bag == s) return {};
+    }
+    checkedBags.push_back(bag);
+    vector<string> out = rules[bag].innerBagCol;
+    int i = -1;
+    while(++i < out.size()) {
+        vector<string> findInnerOut = findInnerBags(out[i]);
+        for(string s : findInnerOut) {
+            out.push_back(s);
+        }
+    }
+    return out;
 }
--- 07/main.h
+++ 07/main.h
@@ -7,6 +7,19 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <unordered_map>
 using namespace std;
+
+struct Rule {
+    vector<int> innerBagNum; // parallel
+    vector<string> innerBagCol; // parallel
+    bool empty;
+};
+
+unordered_map<string, Rule> rules;
+vector<string> checkedBags;
+
+vector<string> findInnerBags(string bag);
 
 #endif //ADVENTOFCODE2020_MAIN_H
